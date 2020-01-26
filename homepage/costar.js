febb720d199a062a60ec6cca20deda5221e40ec6
yamidona <joji-kid@outlook.jp>
2020-01-27 03:25:36 +0900

    Update costar.js

--- homepage/costar.js
+++ homepage/costar.js
@@ -1,44 +1,41 @@
-const btn = document.getElementById('btn');
-btn.addEventListener('click', function() {
-  var name = document.getElementById('actor-name').value;
-  var textModMsg = document.getElementById("output");
-  var output1=action(name)
-  textModMsg.textContent =  output1;
-  
-});
 
-
+const pattern0='[\n\r]+';
+const  pattern1='<span class=.*? id="テレビアニメ">テレビアニメ</span>(.*?)<h3>';
+const  pattern3='<span class=.*? id="劇場アニメ">劇場アニメ</span>(.*?)<h3>';
+const  pattern4='<span class=.*? id="OVA">OVA</span>(.*?)<h3>';
+const pattern5='<span class=.*? id="Webアニメ">Webアニメ</span>(.*?)<h3>';
+const pattern2='<li><a href=.*?>(.*?)</a>';
 function UrlChecker(urlname00){
     fetch(urlname00)
         .then(response => {
             
             // エラーレスポンスが返されたことを検知する
             if (!response.ok) {
-                return False
+                return false;
             } else {
-               return True
+               return true;
                
             }
         }).catch(error => {
-            return False
+            return false;
         });
 }
 function match_text_get(pattern1,pattern2,text){
-    var text0=text.match(pattern1);
+    var text1=text.match(pattern1);
     var works2=[];
         if (text1!==[]){
             works2=text1[0].match(pattern2);
         }else{
             works2=[];
         }
-        return work2;
+        return works2;
 }
 function title_get(url){
     
-    
+    console.log("ソース")
     var request = new XMLHttpRequest();
- 
-    request.open('GET', url, true);
+    var works9=[];
+    /*request.open('GET', url, true);
     request.responseType = 'json';
  
     request.onload = function () {
@@ -46,11 +43,13 @@ function title_get(url){
       console.log(data);
     };
  
-    request.send();
-  
+    request.send();*/
+    console.log("ソース");
+    var text=requestsorce(url);
+  　　console.log("ソース"+text);
     text.replace(pattern0,'');
 
-
+  
     var works1=match_text_get(pattern1,pattern2,text);
     var works2=match_text_get(pattern3,pattern2,text);
     var works3=match_text_get(pattern4,pattern2,text);
@@ -60,33 +59,98 @@ function title_get(url){
     works1.push(works4);
 
     for (let i=0;i<works1.length;i++){
-        works9.append(work.replace('&.*?;','&'));
+        works9.append(works1[i].replace('&.*?;','&'));
     }
     return works9;
 }
+function createhttprequest(){
+
+var request=null;
+
+if("XMLHttpRequest" in window){
+
+	request= new XMLHttpRequest();
+        
+
+
+}
+else if("ActiveXObject" in window){
+
+	try{
+
+	request=new ActiveXobject("Msxml2.XMLHTTP");
+	
+
+
+	}catch(e){
+	
+
+		try{
+
+		request=new ActiveXObject("Microsoft.XMLHTTP");
+		
+
+		}catch(e){
+}
+
+	}
+
+}
+return request;
+}
+
+var request;
+
+function requestsorce(url){
+
+request=createhttprequest();
+
+request.open("GET",url,true);
+request.onreadystatechange=sorceget;
+request.send(null);
+}
+
+function sorceget(){
+
+
+if (request.readyState == 4 && request.status == 200){
+	
+document.form1.sorce.value=request.responseText;
+}
+else{
+
+}
+}
 function voice_works(voice_actor) {
-    var pattern0='[\n\r]+';
-    var pattern1='<span class=.*? id="テレビアニメ">テレビアニメ</span>(.*?)<h3>';
-    var pattern3='<span class=.*? id="劇場アニメ">劇場アニメ</span>(.*?)<h3>';
-    var pattern4='<span class=.*? id="OVA">OVA</span>(.*?)<h3>';
-    var pattern5='<span class=.*? id="Webアニメ">Webアニメ</span>(.*?)<h3>';
-    var pattern2='<li><a href=.*?>(.*?)</a>';
+    
     var url='https://ja.wikipedia.org/wiki/'+encodeURIComponent(voice_actor);
     var url1='https://ja.wikipedia.org/wiki/'+encodeURIComponent(voice_actor)+'_'+encodeURIComponent('(声優)');
     var items = [1,2,3,4];
-    var works=[]
+    var works=[];
     console.log("oppai");
     if  (UrlChecker(url1)) {
         works=title_get(url1);
-        console.log(works)
+        console.log(works);
     }else if  (UrlChecker(url)) {
         works=title_get(url);
+   }else{
+     console.log("oppai1");
    }
-  console.log(works)
-  return works
+  console.log(works);
+  return works;
  }
  
-
+function multiple_list(list1,list2){
+    var list=[];
+    for (let i=0;i<list1.length;i++){
+        if (list2.indexOf(list1[i])!=-1){
+          list.push(list1[i]);
+        }
+    }
+    console.log("リスト"+list);
+    return list;
+   
+}
 
 //import requests
 
@@ -104,7 +168,7 @@ function action(name_list1){
   var name_list = name_list1.split(",");
   console.log(name_list);
   
-  var output=""
+  var output="";
   var title_list=[];
   var voice_actor_anime_list=[];
   if (name_list.length>=3) {
@@ -126,10 +190,10 @@ function action(name_list1){
           }
       }
       for (let i=0;i<title_list.length;i++){
-          actor_list=[]
+          var actor_list=[];
           for (let k=0;k<voice_actor_anime_list.length;k++){
               if (voice_actor_anime_list[k].indexOf(title_list[i])){
-                  actor_list.push(name_list[i])
+                  actor_list.push(name_list[i]);
               }
           }
           if (actor_list.length>=2) {
@@ -138,10 +202,19 @@ function action(name_list1){
           }
       }
   }else{
-      title_list0=voice_works(name_list[0]);
-      title_list1=voice_works(name_list[1]);
-      output+=Array.from(title_list0) & Array.from(title_list1);
+      let title_list0=voice_works(name_list[0]);
+      let title_list1=voice_works(name_list[1]);
+      output+=multiple_list(title_list0,title_list1);
   }
   return output;
 }
 
+
+const btn = document.getElementById('btn');
+btn.addEventListener('click', function() {
+  var name = document.getElementById('actor-name').value;
+  var textModMsg = document.getElementById("output");
+  var output1=action(name);
+  textModMsg.textContent =  output1;
+  
+});
